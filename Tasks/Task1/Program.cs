bd5a6969b16905b89f4cd8da413ab10e70b7096a
Mohammad Shakil <shakiltalukder.seu19@gmail.com>
2021-09-28 22:52:20 +0600

    Add project files.

---
+++ Tasks/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = GetName();
+            Console.WriteLine(name);
+        }
+
+        public static string GetName()
+        {
+            // Remove NotImplementedException and add code here.
+            // GetObject returns an object which has Name property
+            // Get that value and return that.
+            dynamic Names = GetObject();         
+            return Names.Name;
+        }
+
+        public static object GetObject()
+        {
+            return new { Name = "Camera", Price = 200 };
+        }
+    }
+}
